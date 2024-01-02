db2a8b65873da33d8ee653ad5732928fe81ba3bb
Sathiyanarayanan G <84400233+G-SathiyaNarayanan@users.noreply.github.com>
2024-01-02 20:38:09 +0530

    Create Exercise-1.py

---
+++ Exercise-1.py
@@ -0,0 +1,8 @@
+#calculating the Person how many days he live until 90 Years
+
+age=int(input("Enter Your Age: "))
+years= 90- age 
+months=years*12
+weeks=years*52
+days=years*365
+print(f"You will survie {age} years, {months} months, {weeks} weeks and {days} days until 90 years ")
